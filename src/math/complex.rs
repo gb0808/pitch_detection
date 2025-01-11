7e291c969e770095f6b3bd633397863105fef91e
Gabe Barnard <gbarnard08@gmail.com>
2025-01-11 12:01:05 -0500

    impl partial eq for complex numbers

--- src/math/complex.rs
+++ src/math/complex.rs
@@ -3,6 +3,8 @@ pub enum Sign {
     Negative,
 }
 
+#[derive(Copy, Clone)]
+#[derive(Debug)]
 pub struct ComplexNumber {
     pub r: f32,
     pub i: f32
@@ -16,12 +18,25 @@ impl ComplexNumber {
         }
     }
 
+    pub fn from(x: f32) -> Self {
+        Self {
+            r: x,
+            i: 0.0,
+        }
+    }
+
     pub fn get_magnitude(self) -> f32 {
         ((self.r * self.r) + (self.i * self.i)).sqrt()
     }
 
     pub fn get_phase(self) -> f32 {
         (self.i / self.r).atan()
+    }
+}
+
+impl PartialEq for ComplexNumber {
+    fn eq(&self, other: &Self) -> bool {
+        self.r == other.r && self.i == other.i
     }
 }
 
