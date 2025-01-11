a08ea5f7968a33b5a2f2b8adf2ed81cabb8811d4
Gabe Barnard <gbarnard08@gmail.com>
2025-01-11 12:00:35 -0500

    bit_reverse_order now operates on vectors of complex numbers

--- src/math/bits.rs
+++ src/math/bits.rs
@@ -1,4 +1,6 @@
-pub fn bit_reverse_order(arr: &mut [u8]) {
+use super::ComplexNumber;
+
+pub fn bit_reverse_order(arr: &mut Vec<ComplexNumber>) {
     let arr_length = arr.len();
     let bit_length = format!("{:b}", arr_length - 1).len();
     for j in 0..arr_length {
@@ -42,15 +44,20 @@ mod tests {
 
     #[test]
     fn bit_reverse_order_test() {
-        let arr = RefCell::new(vec![0, 1, 2, 3, 4, 5, 6, 7]);
-        bit_reverse_order(&mut arr.borrow_mut());
-        assert_eq!(arr.borrow()[0], 0);
-        assert_eq!(arr.borrow()[1], 4);
-        assert_eq!(arr.borrow()[2], 2);
-        assert_eq!(arr.borrow()[3], 6);
-        assert_eq!(arr.borrow()[4], 1);
-        assert_eq!(arr.borrow()[5], 5);
-        assert_eq!(arr.borrow()[6], 3);
-        assert_eq!(arr.borrow()[7], 7);
+        let mut arr = vec![
+            ComplexNumber::from(0.0), ComplexNumber::from(1.0),
+            ComplexNumber::from(2.0), ComplexNumber::from(3.0),
+            ComplexNumber::from(4.0), ComplexNumber::from(5.0),
+            ComplexNumber::from(6.0), ComplexNumber::from(7.0)
+        ];
+        bit_reverse_order(&mut arr);
+        assert_eq!(arr[0], ComplexNumber::from(0.0));
+        assert_eq!(arr[1], ComplexNumber::from(4.0));
+        assert_eq!(arr[2], ComplexNumber::from(2.0));
+        assert_eq!(arr[3], ComplexNumber::from(6.0));
+        assert_eq!(arr[4], ComplexNumber::from(1.0));
+        assert_eq!(arr[5], ComplexNumber::from(5.0));
+        assert_eq!(arr[6], ComplexNumber::from(3.0));
+        assert_eq!(arr[7], ComplexNumber::from(7.0));
     }
 }
