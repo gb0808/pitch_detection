a60155fd3a626acb7cac3e9c1704ee84111e5bd5
Gabe Barnard <gbarnard08@gmail.com>
2025-01-14 16:07:46 -0600

    using rustfft until custom fft is implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+pub mod math;
 pub mod sound;
+
+use sound::Sound;
+use rustfft::{FftPlanner, num_complex::Complex};
+
+fn detect_pitch(sound: Sound) -> f32 {
+    todo!()    
+}
+
+mod tests {
+    #![allow(unused)]
+    use super::*;
+
+}
