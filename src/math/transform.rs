71c78985f7104a2f871d5352cc691c012385aa76
Gabe Barnard <gbarnard08@gmail.com>
2025-01-15 10:20:27 -0600

    custom fft works - needs to be faster

--- src/math/transform.rs
+++ src/math/transform.rs
@@ -2,9 +2,6 @@ use super::{ComplexNumber, _e, bit_reverse_order, Sign};
 
 const PI: f32 = 3.14159265358979323846264338327950288;
 
-use std::fs::File;
-use std::io::{BufWriter, Write};
-
 fn create_complex_array(a: &[u8]) -> Vec<ComplexNumber> {
     let mut ca = Vec::new();
     for x in a {
@@ -72,37 +69,5 @@ mod tests {
         let mut arr2 = vec![ComplexNumber::default(); 15837];
         pad_data(&mut arr2);
         assert_eq!(arr2.len(), 16384);
-    }
-
-    #[test]
-    fn test_plot_fft() {
-        let sample_rate = 64;
-        let length_of_signal = 64;
-        let f = 3.0;
-
-        let mut data = Vec::new();
-        for i in 0..length_of_signal {
-            let t = i as f32 / sample_rate as f32;
-            data.push(ComplexNumber::from((2.0 * PI * f * t as f32).sin()));
-        }
-        let fft_data = butterflies(&data, 1.0, length_of_signal as f32);
-        let mut magnitues = Vec::new();
-        for cn in fft_data {
-            magnitues.push(cn.get_magnitude())
-        }
-
-        if let Err(e) = write_f32_vector_to_file(
-            "test_out/out.csv", &magnitues, sample_rate) {
-            println!("Error writing to file: {}", e);
-        }
-    }
-
-    fn write_f32_vector_to_file(filename: &str, data: &[f32], sr: u32) -> std::io::Result<()> {
-        let file = File::create(filename)?;
-        let mut writer = BufWriter::new(file);
-        for i in 0..data.len() {
-            writeln!(&mut writer, "{}, {},", i as f32 / data.len() as f32 * sr as f32, data[i])?;
-        }
-        Ok(())
     }
 }
