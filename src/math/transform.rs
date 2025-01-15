0cfe6e402b9e41760b225ddb5283080b1baa2afb
Gabe Barnard <gbarnard08@gmail.com>
2025-01-15 13:40:56 -0600

    fft is done inplace

--- src/math/transform.rs
+++ src/math/transform.rs
@@ -27,34 +27,39 @@ fn _w(k: f32, n: f32) -> ComplexNumber {
     _e(Sign::Negative, 2.0 * PI * k / n)
 }
 
-fn butterflies(a: &[ComplexNumber], k: f32, n: f32) -> Vec<ComplexNumber> {
-    if a.len() == 2 {
-        let p = a[0];
-        let q = a[1];
-        return vec![p.add(&_w(0.0, n).mul(&q)), p.sub(&_w(k, n).mul(&q))]
+fn butterflies(a: &mut [ComplexNumber]) {
+    let mut w = vec![ComplexNumber::default(); a.len()];
+    let (mut xhight, mut k) = (1, a.len() / 2);
+    while k > 0 {
+        for i in 0..a.len() {
+            if i + xhight >= a.len() {
+                break;
+            }
+
+            let (p, q) = (a[i], a[i + xhight]);
+            let (x, y) = (((k * i) % a.len()), (k * i + xhight) % a.len());
+            if w[x] == ComplexNumber::default() {
+                w[x] = _e(Sign::Negative, 2.0 * PI * x as f32 / a.len() as f32);
+            }
+            if w[y] == ComplexNumber::default() {
+                w[y] = _e(Sign::Negative, 2.0 * PI * y as f32 / a.len() as f32);
+            }
+
+            a[i] = p.add(&w[x].mul(&q));
+            a[i + xhight] = p.add(&w[y].mul(&q));
+
+            xhight *= 2;
+            k /= 2;
+        }
     }
-
-    let a1 = butterflies(&a[0..a.len()/2], k * 2.0, n);
-    let a2 = butterflies(&a[a.len()/2..], k * 2.0, n);
-
-    let mut ap1: Vec<ComplexNumber> = Vec::new();
-    let mut ap2: Vec<ComplexNumber> = Vec::new();
-
-    for i in 0..a1.len() {
-        let p = a1[i];
-        let q = a2[i];
-        ap1.push(p.add(&_w(k * i as f32, n).mul(&q)));
-        ap2.push(p.sub(&_w(k * i as f32, n).mul(&q)));
-    }
-    
-    [&ap1[..], &ap2[..]].concat()
 }
 
 pub fn fast_fourier_transform(data: &[u8]) -> Vec<ComplexNumber> {
     let mut _data = create_complex_array(data);
     pad_data(&mut _data);
     bit_reverse_order(&mut _data);
-    butterflies(&_data, 1.0, _data.len() as f32)
+    butterflies(&mut _data);
+    _data
 }
 
 mod tests {
