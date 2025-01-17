13244af0abf3e7e0dee0f971befd6177a3e470e2
Gabe Barnard <gbarnard08@gmail.com>
2025-01-17 16:28:26 -0600

    added moving notes to test cases

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,24 @@ mod tests {
     use dsp::*;
     use sound::Sound;
 
+    #[ignore]
     #[test]
-    fn test_hps() {
+    fn test_hps_sine() {
         let test_freq = 440;
         let sound = Sound::from_wav(&format!("./sounds/sine_waves/{test_freq}.wav"));
         let frequencies = dsp::detect_pitch(&sound.data, sound.sample_rate);
         for freq in frequencies {
             println!("{freq}");
         }
+    }
+
+    #[ignore]
+    #[test]
+    fn test_hps_scale() {
+        let sound = Sound::from_wav("./sounds/scales/c_major_scale.wav");
+        let frequencies = dsp::detect_pitch(&sound.data, sound.sample_rate);
+        for freq in frequencies {
+            println!("{freq}");
+        } 
     }
 }
