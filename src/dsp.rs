6ed8f751f06f79e33cfdf7fd9d9028cfbc36f563
Gabe Barnard <gbarnard08@gmail.com>
2025-01-17 11:01:00 -0600

    moved signal processing code to its own module

---
+++ src/dsp.rs
@@ -0,0 +1,55 @@
+use rustfft::{FftPlanner, num_complex::Complex};
+
+fn divide_signal(signal: &[u8], frame_size: usize) -> Vec<Vec<u8>> {
+    let mut data = Vec::new();
+    let mut i = 0;
+    while i < signal.len() {
+        let remaining_slots = signal.len() - i;
+        if remaining_slots >= frame_size {
+            data.push(signal[i..i+frame_size].to_vec());
+        } else {
+            data.push([&signal[i..], &vec![0u8; frame_size - remaining_slots]].concat().to_vec());
+        }
+        i += frame_size;
+    }
+    data
+}
+
+fn run_transform(data: &[u8]) -> Vec<Complex<f32>> {
+    let mut planner = FftPlanner::new();
+    let fft = planner.plan_fft_forward(data.len());
+    let mut buffer: Vec<Complex<f32>> = data.into_iter()
+                                            .map(|sample| Complex::new(*sample as f32, 0.0))
+                                            .collect();
+    fft.process(&mut buffer);
+    buffer
+}
+
+fn harmonic_product_spectrum(data: &[Complex<f32>], sample_rate: u32) -> f32 {
+    const R: u8 = 1;
+    let low = (20.0 * data.len() as f32 / sample_rate as f32) as usize;
+    let high = (20000.0 * data.len() as f32 / sample_rate as f32) as usize;
+    let (mut max_frequency, mut max_product) = (0.0, 0.0);
+    for i in low..high {
+        let mut product = 1.0;
+        let freq = i as f32 / data.len() as f32 * sample_rate as f32;
+        for r in 1..=R {
+            let index = (freq * r as f32 * data.len() as f32 / sample_rate as f32) as usize;
+            product *= data[index].norm();
+        }
+        if product > max_product {
+            max_product = product;
+            max_frequency = freq;
+        }
+    }
+    max_frequency * 2.0
+}
+
+pub fn detect_pitch(signal: &[u8], sample_rate: u32) -> Vec<f32> {
+    let raw_frames = divide_signal(signal, 88200);
+    println!("Signal Length: {}", raw_frames[1].len());
+    let frames: Vec<Vec<Complex<f32>>> = raw_frames.into_iter().map(|raw_frame| {
+        raw_frame.into_iter().map(|sample| Complex::new(sample as f32, 0.0)).collect()
+    }).collect();
+    frames.into_iter().map(|frame| harmonic_product_spectrum(&frame, sample_rate)).collect()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,19 @@
 pub mod sound;
-
-use sound::Sound;
-use rustfft::{FftPlanner, num_complex::Complex};
-
-fn run_transform(data: &[u8]) -> Vec<Complex<f32>> {
-    let mut planner = FftPlanner::new();
-    let fft = planner.plan_fft_forward(data.len());
-    let mut buffer = Vec::new();
-    for sample in data {
-        buffer.push(Complex::new(*sample as f32, 0.0));
-    }
-    fft.process(&mut buffer);
-    buffer
-}
-
-fn harmonic_product_spectrum(data: &[Complex<f32>], sample_rate: u32) -> f32 {
-    const R: u8 = 1;
-    let low = (20.0 * data.len() as f32 / sample_rate as f32) as usize;
-    let high = (20000.0 * data.len() as f32 / sample_rate as f32) as usize;
-    let (mut max_frequency, mut max_product) = (0.0, 0.0);
-    for i in low..high {
-        let mut product = 1.0;
-        let freq = i as f32 / data.len() as f32 * sample_rate as f32;
-        for r in 1..=R {
-            let index = (freq * r as f32 * data.len() as f32 / sample_rate as f32) as usize;
-            product *= data[index].norm();
-        }
-        if product > max_product {
-            max_product = product;
-            max_frequency = freq;
-        }
-    }
-    max_frequency * 2.0
-}
-
-fn detect_pitch(sound: Sound) -> f32 {
-    let data = run_transform(&sound.data);
-    let mut norms = Vec::new();
-    for slice in &data {
-        norms.push(slice.norm());
-    }
-    harmonic_product_spectrum(&data, sound.sample_rate)
-}
+pub mod dsp;
 
 mod tests {
     #![allow(unused)]
     use super::*;
-
-    macro_rules! approx {
-        ($x:expr, $y:expr) => {
-            if !($x - $y < 0.05 && $y - $x < 0.05) {
-                panic!();
-            }
-        };
-    }
+    use dsp::*;
+    use sound::Sound;
 
     #[test]
     fn test_hps() {
-        let sound1 = Sound::from_wav("./sounds/sine_waves/220.wav");
-        let freq = detect_pitch(sound1);
-        approx!(220.0, freq);
-
-        let sound2 = Sound::from_wav("./sounds/sine_waves/440.wav");
-        let freq = detect_pitch(sound2);
-        approx!(440.0, freq);
-
-        let sound3 = Sound::from_wav("./sounds/sine_waves/660.wav");
-        let freq = detect_pitch(sound3);
-        approx!(660.0, freq);
+        let test_freq = 440;
+        let sound = Sound::from_wav(&format!("./sounds/sine_waves/{test_freq}.wav"));
+        let frequencies = dsp::detect_pitch(&sound.data, sound.sample_rate);
+        for freq in frequencies {
+            println!("{freq}");
+        }
     }
 }
